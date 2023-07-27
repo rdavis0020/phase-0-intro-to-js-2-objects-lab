19763bf0161e4f482ec9e27a68d87fb1f4fecc54
Ryan Davis <rdavis0020@gmail.com>
2023-07-27 17:07:52 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+  name: "Ryan Davis",
+  streetAddress: "123 Main Street",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = {
+    ...employee,
+    [key]: value,
+  };
+
+  return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeClone = { ...employee };
+  delete employeeClone[key];
+  return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
